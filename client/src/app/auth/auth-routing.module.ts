ad183ca94294fa54c00ad7d06a4123c7b2cf6f34
Sonam <son1@publicisgroupe.net>
2021-02-24 09:58:43 +0530

    sign up and sign in form

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LOGIN, REGISTER } from '../constants/routes.constants';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: LOGIN.name,
+    component: LoginComponent
+  },
+  {
+    path: REGISTER.name,
+    component: RegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
